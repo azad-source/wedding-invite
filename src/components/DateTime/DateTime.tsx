afff32d7b94394abb3aceef25a23022770891bf9
Azad <azad.63.mamedov@gmail.com>
2022-08-08 00:33:38 +0400

    Таймер

--- src/components/DateTime/DateTime.tsx
+++ src/components/DateTime/DateTime.tsx
@@ -1,4 +1,5 @@
 import { PageWrapper } from 'components/layout/PageWrapper';
+import { Timer } from 'components/Timer/Timer';
 import * as React from 'react';
 import styles from './DateTime.module.scss';
 
@@ -9,7 +10,7 @@ export const DateTime = () => {
                 <div className={styles.root__title}>Дорогие родные и друзья!</div>
                 <div className={styles.root__description}>
                     <div>
-                        Приглашаем Вас на торжество, посвещенное нашему&raquo;бракосочетанию которое
+                        Приглашаем Вас на торжество, посвещенное нашему&nbsp;бракосочетанию которое
                         состоится
                     </div>
                     <div>15 октября 2022 года</div>
@@ -19,6 +20,7 @@ export const DateTime = () => {
                         по&nbsp;адресу&nbsp;г.Тольятти ул. Комзина 6, зал &laquo;Зеркальный&raquo;
                     </div>
                 </div>
+                <Timer className={styles.root__timer} date={new Date(2022, 9, 15, 17, 0, 0)} />
             </div>
         </PageWrapper>
     );
---
+++ src/components/Timer/Timer.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import cn from 'classnames';
+import styles from './Timer.module.scss';
+
+interface Props {
+    date: Date;
+    className: string | undefined;
+}
+
+export const Timer: React.FC<Props> = ({ date, className }) => {
+    const [timer, setTimer] = React.useState<{
+        days: number;
+        hours: number;
+        minutes: number;
+        seconds: number;
+    }>({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            const deltaMilliseconds = date.getTime() - new Date().getTime();
+            const days = deltaMilliseconds / (1000 * 3600 * 24);
+            const daysInt = Math.floor(days);
+            const hours = (days - daysInt) * 24;
+            const hoursInt = Math.floor(hours);
+            const minutes = (hours - hoursInt) * 60;
+            const minutesInt = Math.floor(minutes);
+            const seconds = (minutes - minutesInt) * 60;
+            const secondsInt = Math.floor(seconds);
+
+            setTimer({
+                days: daysInt,
+                hours: hoursInt,
+                minutes: minutesInt,
+                seconds: secondsInt,
+            });
+        }, 1000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    return (
+        <div className={cn(styles.root, className)}>
+            <div className={cn(styles.root__item, styles.root__days)}>
+                <div className={styles.root__item__value}>{timer.days}</div>
+                <div className={styles.root__item__caption}>Дней</div>
+            </div>
+            <div className={cn(styles.root__item, styles.root__hours)}>
+                <div className={styles.root__item__value}>{timer.hours}</div>
+                <div className={styles.root__item__caption}>Часов</div>
+            </div>
+            <div className={cn(styles.root__item, styles.root__minutes)}>
+                <div className={styles.root__item__value}>{timer.minutes}</div>
+                <div className={styles.root__item__caption}>Минут</div>
+            </div>
+            <div className={cn(styles.root__item, styles.root__seconds)}>
+                <div className={styles.root__item__value}>{timer.seconds}</div>
+                <div className={styles.root__item__caption}>Секунд</div>
+            </div>
+        </div>
+    );
+};
